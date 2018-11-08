ebc86f21cba70a4205a4276e6a2a772199982ed6
luohu <luohu@huoyunren.com>
2018-11-08 22:28:11 +0800

    complete tool

--- Main.lua
+++ Main.lua
@@ -1,14 +1,14 @@
-require("./Libs/ALibStub/LibStub.lua")
 -- app global vars
 PVPTools = LibStub("AceAddon-3.0"):NewAddon("PVPTools", "AceConsole-3.0", "AceEvent-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("PVPTools")
 local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 local AceGUI = LibStub("AceGUI-3.0")
 local AppName = "PVPTools"
-local BtnFrame, BtnEnemy, BtnNotifyStealth
+local BtnFrame, BtnSafe, BtnEnemy, BtnNotifyStealth
 
 BINDING_HEADER_PVPTools = "PVPTools";
 BINDING_NAME_PVPTools_Enemy_Coming = L["keyEnemyComing"]
+
 
 -- default config
 local options = {
@@ -16,20 +16,62 @@ local options = {
     handler = PVPTools,
     type = "group",
     args = {
-        notifyStealth = {
+        showMessageAuto = {
             type = "toggle",
-            name = L["notifyStealth"],
-            desc = L["toggleNotify"],
+            name = L["showMessageAuto"],
+            desc = L["textDhowMessageAuto"],
             get = function(info)
-                return PVPInfo.db.profile.notifyStealth;
+                return PVPTools.db.profile.showMessageAuto
             end,
             set = function(info, value)
-                if value == true then
-                    BtnNotifyStealth:Show()
-                else
-                    BtnNotifyStealth:Hide()
-                end
-                PVPInfo.db.profile.notifyStealth = value or false;
+                PVPTools.db.profile.showMessageAuto = value or false
+            end,
+        },
+        isPlayer = {
+            type = "toggle",
+            name = L["isPlayer"],
+            desc = L["textIsPlayer"],
+            get = function(info)
+                return PVPTools.db.profile.isPlayer
+            end,
+            set = function(info, value)
+                PVPTools.db.profile.isPlayer = value or false
+            end,
+        },
+        enemyCache = {
+            type = "toggle",
+            name = L["enemyCache"],
+            desc = L["textEnemyCache"],
+            get = function(info)
+                return PVPTools.db.profile.enemyCache
+            end,
+            set = function(info, value)
+                PVPTools.db.profile.enemy = {}
+                PVPTools.db.profile.enemyCache = value or false
+            end,
+        },
+        top = {
+            type = "input",
+            name = "top",
+            desc = L["top"],
+            get = function(info)
+                return PVPTools.db.profile.frameTop
+            end,
+            set = function(info, value)
+                PVPTools.db.profile.frameTop = value or -540
+                BtnFrame:SetPoint("CENTER", UIParent, "CENTER", PVPTools.db.profile.frameLeft, PVPTools.db.profile.frameTop)
+            end,
+        },
+        left = {
+            type = "input",
+            name = "left",
+            desc = L["left"],
+            get = function(info)
+                return PVPTools.db.profile.frameLeft
+            end,
+            set = function(info, value)
+                PVPTools.db.profile.frameLeft = value or 550
+                BtnFrame:SetPoint("CENTER", UIParent, "CENTER", PVPTools.db.profile.frameLeft, PVPTools.db.profile.frameTop)
             end,
         },
     },
@@ -38,6 +80,12 @@ local options = {
 local defaults = {
     profile = {
         notifyStealth = false,
+        enemy = {},
+        showMessageAuto = true,
+        isPlayer = true,
+        enemyCache = false,
+        frameTop = -540,
+        frameLeft = 550,
     },
 }
 
@@ -57,179 +105,146 @@ function PVPTools:ShowConfig()
 end
 
 function PVPTools:OnEnable()
+    PVPTools.db.profile.enemy = {}
     PVPTools_Init_Frame()
-
-    --self:RegisterEvent("NAME_PLATE_UNIT_ADDED");
-    --self:RegisterEvent("NAME_PLATE_UNIT_REMOVED");
-    --self:RegisterEvent("PLAYER_TARGET_CHANGED");
-    self:RegisterEvent("PLAYER_REGEN_ENABLED")
-    self:RegisterEvent("PLAYER_REGEN_DISABLED");
-
-    --self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
-    --self:RegisterEvent("COMBAT_LOG_EVENT");
-
-    self:RegisterEvent("PLAYER_ENTERING_WORLD");
-    self:RegisterEvent("ZONE_CHANGED_NEW_AREA");
-
-    self:RegisterEvent("ARENA_OPPONENT_UPDATE");
-    self:RegisterEvent("ARENA_PREP_OPPONENT_SPECIALIZATIONS");
-    self:RegisterEvent("UPDATE_BATTLEFIELD_SCORE");
-    self:RegisterEvent("UPDATE_BATTLEFIELD_STATUS");
-    self:RegisterEvent("UPDATE_MOUSEOVER_UNIT");
 end
 
-function PVPTools:PLAYER_TARGET_CHANGED()
-    self:Print("PLAYER_TARGET_CHANGED");
+function SendMessageByUnit(unit)
+    --print("SendMessageByUnit:" .. unit)
+    local unitName, _ = UnitName(unit)
+    local className, _ = UnitClass(unit)
 
-    if not UnitIsVisible("target") then
-        return ;
+    if PVPTools.db.profile.enemyCache == true then
+        if unit ~= "target" or unit ~= "mouseover" then
+            local flag = unitName .. "-" .. className
+            for _, v in pairs(PVPTools.db.profile.enemy) do
+                if v == flag then
+                    return
+                end
+            end
+            table.insert(PVPTools.db.profile.enemy, flag)
+        end
     end
-
-    local guid = UnitGUID("target")
-    if not guid and guid == "" then
-        return ;
+    local zone = GetSubZoneText()
+    if not zone then
+        return
     end
+    local notifyText = L["textEnemy"] .. "「" .. unitName .. "-" .. className .. "」" .. L["textNotifyEnemy"] .. "「" .. zone .. "」," .. L["textHelp"];
 
-    self:Print("GUID:" .. guid);
+    SendMessage(notifyText)
 end
 
-function PVPTools:COMBAT_LOG_EVENT()
-    self:Print("COMBAT_LOG_EVENT")
+function SendMessage(notifyText)
+    if PVPTools.db.profile.showMessageAuto == true then
+        if UnitInRaid("player") then
+            --SendChatMessage(notifyText, "RAID");
+            SendChatMessage(notifyText, "INSTANCE_CHAT");
+        elseif UnitInParty("player") then
+            SendChatMessage(notifyText, "PARTY");
+        else
+            SendChatMessage(notifyText, "SAY");
+        end
+    else
+        PVPTools:Print(notifyText);
+    end
+end
+
+function PVPTools_Enemy_Coming(unit)
+    --print("PVPTools_Enemy_Coming:" .. unit)
+    local unitName, _ = UnitName(unit)
+    local className, _ = UnitClass(unit)
+    if PVPTools.db.profile.isPlayer == true then
+        if not UnitIsPlayer(unit) then
+            return
+        end
+    end
+    if not UnitIsEnemy("player", unit) then
+        return
+    end
+    local zone = GetSubZoneText()
+    if not zone then
+        return
+    end
+    local notifyText = L["textEnemy"] .. "「" .. unitName .. "-" .. className .. "」" .. L["textNotifyEnemy"] .. "「" .. zone .. "」," .. L["textHelp"];
+
+    SendMessage(notifyText)
 end
 
 function PVPTools:NAME_PLATE_UNIT_ADDED(_, unitToken)
-    self:Print("NAME_PLATE_UNIT_ADDED")
+    --self:Print("NAME_PLATE_UNIT_ADDED")
 
     if self.db.profile.notifyStealth ~= true then
         return
     end
     local unit = unitToken;
-    --if not UnitIsVisible(unit) then
-    --    return
-    --end
-    if not UnitIsPlayer(unit) then
+
+    if PVPTools.db.profile.isPlayer == true and not UnitIsPlayer(unit) then
         return
     end
     if not UnitIsEnemy("player", unit) then
         return
     end
-
-    SendMessage(unit)
-end
-
-function PVPTools:PLAYER_REGEN_ENABLED()
-    self:Print("PLAYER_REGEN_ENABLED")
-end
-
-function PVPTools:PLAYER_REGEN_DISABLED()
-    self:Print("PLAYER_REGEN_DISABLED")
-end
-
-function PVPTools:PLAYER_ENTERING_WORLD()
-    self:Print("PLAYER_ENTERING_WORLD")
-end
-
-function PVPTools:ZONE_CHANGED_NEW_AREA()
-    self:Print("ZONE_CHANGED_NEW_AREA")
-end
-
-function PVPTools:ARENA_OPPONENT_UPDATE()
-    self:Print("ARENA_OPPONENT_UPDATE")
-end
-
-function PVPTools:ARENA_PREP_OPPONENT_SPECIALIZATIONS()
-    self:Print("ARENA_PREP_OPPONENT_SPECIALIZATIONS")
-end
-
-function PVPTools:UPDATE_BATTLEFIELD_SCORE()
-    self:Print("UPDATE_BATTLEFIELD_SCORE")
-end
-
-function PVPTools:UPDATE_BATTLEFIELD_STATUS()
-    self:Print("UPDATE_BATTLEFIELD_STATUS")
-end
-
-function PVPTools:UPDATE_MOUSEOVER_UNIT()
-    self:Print("UPDATE_MOUSEOVER_UNIT")
-end
-
-function SendMessage(unit)
-    unitName, unitRealName = UnitName(unit)
-    if unitRealName == nil then
-        unitRealName = GetRealmName()
+    local className, classFileName, _ = UnitClass(unitToken)
+    if classFileName == "HUNTER" and classFileName == "ROGUE" and classFileName == "DRUID" then
+        if CheckInteractDistance("target", 1) then
+            SendMessageByUnit(unit)
+        end
     end
-    className, classFileName = UnitClass(unit)
 
-    local notifyText = L["textEnemy"] .. "「" .. unitName .. " - " .. unitRealName .. " - " .. className .. "」" .. L["textNotifyEnemy"] .. GetSubZoneText() .. L["textHelp"];
-    if UnitInRaid("player") then
-        SendChatMessage(notifyText, "RAID");
-        SendChatMessage(notifyText, "INSTANCE_CHAT");
-    elseif UnitInParty("player") then
-        SendChatMessage(notifyText, "PARTY");
-    else
-        SendChatMessage(notifyText, "");
-    end
-end
-
-function PVPTools_Enemy_Coming()
-    local unit = "mouseover";
-    --if not UnitIsVisible(unit) then
-    --    return
-    --end
-    if not UnitIsPlayer(unit) then
-        return
-    end
-    if not UnitIsEnemy("player", unit) then
-        return
-    end
-    SendMessage(unit)
 end
 
 function PVPTools_Init_Frame()
     -- Create a container frame
-    BtnFrame = AceGUI:Create("Frame")
-    BtnFrame:SetCallback("OnClose", function(widget)
-        AceGUI:Release(widget)
-    end)
-    BtnFrame:SetTitle("PVPTools")
-    BtnFrame:SetStatusText("Status Text")
+    BtnFrame = AceGUI:Create("ScrollFrame")
+    BtnFrame:SetWidth(300)
+    BtnFrame:SetHeight(100)
     BtnFrame:SetLayout("Flow")
-    BtnFrame:SetScript("OnMouseDown", function(frame)
-        frame:StartMoving();
-    end)
-    BtnFrame:SetScript("OnMouseUp", function(frame)
-        frame:StopMovingOrSizing()
-    end)
-    BtnFrame:Show()
+    BtnFrame:SetPoint("CENTER", UIParent, "CENTER", PVPTools.db.profile.frameLeft, PVPTools.db.profile.frameTop)
+    --end
 
-    local BtnSafe = AceGUI:Create("Button")
-    BtnSafe:SetWidth(30)
+    BtnSafe = AceGUI:Create("Button")
+    BtnSafe:SetWidth(70)
     BtnSafe:SetText(L["textSafe"])
-    BtnSafe:SetCallback("OnClick", PVPTools_Enemy_Coming)
-    BtnSafe:AddChild(BtnSafe)
+    BtnSafe:SetCallback("OnClick", function()
+        local zone = GetSubZoneText()
+        if zone then
+            local notifyText = "【" .. zone .. "】已安全，两点之间的机动点留人随时支援！"
+            SendMessage(notifyText)
+        end
+    end)
+    BtnFrame:AddChild(BtnSafe)
 
     BtnEnemy = AceGUI:Create("Button")
-    BtnEnemy:SetWidth(30)
+    BtnEnemy:SetWidth(70)
     BtnEnemy:SetText(L["textEnemy"])
-    BtnEnemy:SetCallback("OnClick", PVPTools_Enemy_Coming)
+    BtnEnemy:SetCallback("OnClick", function()
+        PVPTools_Enemy_Coming("target")
+    end)
     BtnFrame:AddChild(BtnEnemy)
 
     BtnNotifyStealth = AceGUI:Create("Button")
-    BtnNotifyStealth:SetAutoWidth(true)
+    BtnNotifyStealth:SetWidth(90)
+    if PVPTools.db.profile.notifyStealth == false then
+        BtnNotifyStealth:SetText(L["textStealth"] .. ":" .. L["close"])
+        PVPTools:UnregisterEvent("NAME_PLATE_UNIT_ADDED");
+    else
+        BtnNotifyStealth:SetText(L["textStealth"] .. ":" .. L["open"])
+        PVPTools:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+    end
     BtnNotifyStealth:SetCallback("OnClick", function()
+        if PVPTools.db.profile.enemyCache == true then
+            PVPTools.db.profile.enemy = {}
+        end
         local flag = PVPTools.db.profile.notifyStealth
         if flag == true then
             BtnNotifyStealth:SetText(L["textStealth"] .. ":" .. L["close"])
+            PVPTools:UnregisterEvent("NAME_PLATE_UNIT_ADDED");
             PVPTools.db.profile.notifyStealth = false
-            self:UnregisterEvent("NAME_PLATE_UNIT_ADDED");
         else
             BtnNotifyStealth:SetText(L["textStealth"] .. ":" .. L["open"])
             PVPTools.db.profile.notifyStealth = true
-            self:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+            PVPTools:RegisterEvent("NAME_PLATE_UNIT_ADDED")
         end
     end)
     BtnFrame:AddChild(BtnNotifyStealth)
-    if PVPTools.db.profile.notifyStealth == false then
-        BtnNotifyStealth:Hide()
-    end
 end
